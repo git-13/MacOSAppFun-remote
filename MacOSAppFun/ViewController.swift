93fe1aafc853fd4f07ecd6e6b1df881370bca343
Vladislav <git13vl@gmail.com>
2022-06-11 09:46:28 +0200

    Initial Commit

---
+++ MacOSAppFun/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacOSAppFun
+//
+//  Created by Vladislav Glovatskiy on 11.06.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
